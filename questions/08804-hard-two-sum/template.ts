9505625bf216e7cda0e4b1c50d7fbd19e625128b
pumkko <c.coque@hotmail.fr>
2024-02-28 13:47:38 -0500

    complete hard two sum
    "

--- questions/08804-hard-two-sum/template.ts
+++ questions/08804-hard-two-sum/template.ts
@@ -1,1 +1,15 @@
-type TwoSum<T extends number[], U extends number> = any
+type ArrayOfSize<Size extends number, TArray extends number[] = []>
+    = TArray['length'] extends Size ? TArray : ArrayOfSize<Size, [0, ...TArray]>
+
+type SumNumbers<A extends number, B extends number> = [...ArrayOfSize<A>, ...ArrayOfSize<B>]['length']
+
+
+type TwoSum<T extends number[], U extends number> =
+    T extends [infer F extends number, infer S extends number, ...infer Tail extends number[]]
+        ? SumNumbers<F, S> extends U
+            ? true
+            : TwoSum<[F, ...Tail], U> extends true 
+                ? true 
+                : TwoSum<[S, ...Tail], U>
+
+    : false
